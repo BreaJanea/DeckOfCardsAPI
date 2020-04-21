abc1f5df0fe4039a24f9bc6b642703651e29388a
Brea Spencer <breaspencer@quickenloans.com>
2020-04-21 09:05:00 -0400

    first commit

---
+++ DeckOfCardsAPI/Models/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckOfCardsAPI.Models
+{
+    public class Deck
+    {
+        public string DeckID { get; set; }
+        public Card[] Cards { get; set; }
+    }
+
+    public class Card
+    {
+       public Uri Image { get; set; }
+       public string Value { get; set; }
+       public string Suit { get; set; }
+    }
+}
